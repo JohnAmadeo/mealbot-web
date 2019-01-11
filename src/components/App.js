09106a4f209f593eadd77a8216244b7d40681890
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2019-01-11 13:06:26 +0700

    Refactor routes handling + setup global styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,89 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 
 import Auth from '../auth/auth';
+import CrossMatchTraitContainer from '../containers/CrossMatchTraitContainer';
 import DashboardContainer from '../containers/DashboardContainer';
 import MembersContainer from '../containers/MembersContainer';
-import PairingContainer from '../containers/PairingContainer';
+import PairsContainer from '../containers/PairsContainer';
 import Landing from './Landing';
 import Callback from './Callback';
+import RoundsContainer from '../containers/RoundsContainer';
+import ErrorListContainer from '../containers/ErrorListContainer';
 
-console.log('starting');
 const auth = new Auth();
+const GlobalStyle = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Playfair+Display:400,700,900');
+    margin: 0;
+  }
 
-class App extends Component {
+  * {
+    box-sizing: border-box;
+    font-family: Helvetica, sans-serif;
+    font-weight: lighter;
+  }
+`;
+
+
+class App extends React.Component {
   render() {
+    const routes = [
+      {
+        component: <MembersContainer auth={auth} />,
+        path: 'members',
+        title: 'Members',
+      },
+      {
+        component: <PairsContainer auth={auth} />,
+        path: 'pairs',
+        title: 'Pairs',
+      },
+      {
+        component: <CrossMatchTraitContainer auth={auth} />,
+        path: 'crossmatchtrait',
+        title: 'Cross Match Trait',
+      },
+      {
+        component: <RoundsContainer auth={auth} />,
+        path: 'rounds',
+        title: 'Rounds',        
+      }
+    ];
+
     return (
-      <BrowserRouter>
-        <>
-          <Route path="/" exact render={props => <Landing auth={auth}/>} />
-          <Route 
-            path="/members"
-            exact
-            // TODO: Wrap MembersContainer in Dashboard component
-            // encapsulates left nav & error list 
-            render={props => 
-              <DashboardContainer auth={auth}>
-                <MembersContainer auth={auth}/>
-              </DashboardContainer> 
-            } 
-            />
-          <Route 
-            path="/pairing" 
-            exact 
-            render={props =>
-              <DashboardContainer auth={auth}>
-                <PairingContainer auth={auth} />
-              </DashboardContainer> 
-            } />
-          <Route 
-            path="/callback" 
-            exact
-            render={props => <Callback auth={auth} {...props}/>} 
-            />
-        </>
-      </BrowserRouter>
+      <>
+        {/* ugly styled-components API; semantically confusing in React */}
+        <GlobalStyle/>
+        <BrowserRouter>
+          <>
+            <Route path="/" exact render={props => <Landing auth={auth}/>} />
+            {routes.map((route, idx) => (
+              <Route
+                key={idx}
+                path={`/${route.path}`}
+                exact
+                render={props =>
+                  <DashboardContainer
+                    auth={auth}
+                    routes={routes}
+                    selectedRouteId={idx}
+                  >
+                    <ErrorListContainer />
+                    {route.component}
+                  </DashboardContainer>
+                }
+                />
+            ))}
+            <Route 
+              path="/callback" 
+              exact
+              render={props => <Callback auth={auth} {...props}/>} 
+              />
+          </>
+        </BrowserRouter>
+      </>
     );
   }
 }
